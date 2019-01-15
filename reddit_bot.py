d8dc733c5327ab535bd822d346296fe228b51b69
ddeniz001 <38838004+ddeniz001@users.noreply.github.com>
2019-01-15 13:59:09 -0500

    Set up the api

---
+++ reddit_bot.py
@@ -0,0 +1,17 @@
+import praw
+
+
+reddit = praw.Reddit(client_id = "U4-FjLqQ9G00kw",
+                client_secret= "QGOer2R8Vws4BjzYjYzxOGqphM0",
+                user_agent = "reddit_bot",
+                password = "fener93250991",
+                username = "hoppazipla"
+             )
+
+
+common_spammy_words = ["Donald Trump Gold Coin", 
+                        "Fidget Spinner",
+                        "Ring","Gold Ring", 
+                        "Stainless"]
+
+
